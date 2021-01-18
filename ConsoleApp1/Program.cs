dce6125ed930dc5fbc214fd3ca71572bae982251
Allan Epifanio <allanstefanin@gmail.com>
2021-01-18 15:25:25 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaUserControl.Domain.Models;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("Allan", "allanstefanin@gmail.com", "allanse");
+
+            Console.WriteLine(user.Name);
+            Console.ReadKey();
+        }
+    }
+}
