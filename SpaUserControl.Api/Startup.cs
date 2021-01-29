c946461a19b04e166bcd3534e6f54d44950f7c5f
Allan Epifanio <allanstefanin@gmail.com>
2021-01-29 12:12:59 -0300

    Aula 06

---
+++ SpaUserControl.Api/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Owin;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
+using SpaUserControl.Api.Helpers;
+using SpaUserControl.Startup;
+using Unity;
+
+namespace SpaUserControl.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            var container = new UnityContainer();
+            DependencyResolver.Resolve(container);
+            config.DependencyResolver = new UnityResolver(container);
+
+            ConfigureWebApi(config);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+        }
+    }
+}
